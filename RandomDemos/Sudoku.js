2e74d85b12f5d60bd021ae583ba9afd7b06b5846
BeastlyKoboi <74743089+BeastlyKoboi@users.noreply.github.com>
2023-04-30 22:25:39 -0400

    Added beginning of populate square

--- RandomDemos/Sudoku.js
+++ RandomDemos/Sudoku.js
@@ -1,9 +1,34 @@
+const populate9Square = (grid, xStart, yStart, Checked) => {
+
+    if (Checked) {
+
+    }
+    else {
+        let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let randIndex = 0;
+
+        for (let x = xStart; x < 3; x++) {
+            for (let y = yStart; y < 3; y++) {
+                randIndex = Math.floor(Math.random() * nums.length);
+
+                grid[x][y] = nums[randIndex];
+
+                nums.splice(randIndex, 1);
+            }
+        }
+
+
+    }
+}
 
 const createSudoku = () => {
     let grid = [9][9];
 
-    
+    populate9Square(grid, 0, 0, false);
+    populate9Square(grid, 3, 3, false);
+    populate9Square(grid, 6, 6, false);
 
+    console.log(grid);
 }
 
 
